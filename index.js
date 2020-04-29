af460c167a3233509885e4ae42e0186ada80b14b
lyq1223 <56785439+lyq1223@users.noreply.github.com>
2020-04-29 23:22:19 +0800

    car

--- index.js
+++ index.js
@@ -1,30 +1,39 @@
-// 导入 node http 模块（处理网络）
-const http = require('http');
-// 导入 fs fileSystem 文件系统 处理文件读写
-const fs = require('fs');
-// 1: 前端 ajax 传输数据给后端，后端接受前端数据
-// 2：响应数据给前端
-// baidu.com  ->  一段 html -> 浏览器发现是html 渲染成了页面 -> 
-// MIME
-// text/html  
-// http:127.0.0.1:8081 => 把 ajax.html 内容返回给前端
-const server = http.createServer(
-  function (req, res) {
-    console.log('当前正在请求', req.url)
-    if (req.url.includes('search')) {
-      // search 搜索功能
-      // /      首页功能
-      res.end(req.url.split('?')[1])
-      return;
+var fs = require('fs'); // node file system node 自己的模块
+var path = require('path'); //路径
+// uuid npm 第三方
+// fs.readFile('aa.txt', function(err, data) {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+//   console.log(data.toString());
+// })
+
+function getDirFiles(startPath) {
+    let result = [];
+    // 递归
+    function finder(parentPath) {
+        // console.log(path);
+        let files = fs.readdirSync(parentPath);
+        if (!files.length) return;
+        // console.log(files);
+        files.forEach(function (val, index) {
+            // console.log(val, index);
+            // 目录还是文件
+            let fPath = path.join(parentPath, val);
+            // console.log(fPath);
+            let stats = fs.statSync(fPath);
+            if (stats.isDirectory()) {
+                finder(fPath);
+            }
+            if (stats.isFile()) {
+                result.push(fPath);
+            }
+        })
     }
-    res.writeHead(200, {
-      'Content-Type': 'text/html;charset=utf-8'
-    })
-    const file = fs.readFileSync('./ajax.html', { encoding: 'utf8' })
-    // console.log(file);
-    res.end(file);
-  })
-server.listen(8081, function () {
-  console.log('server is running http:127.0.0.1:8081')
-})
-// server 有可能失败
+    finder(
+        path.join(process.cwd(), startPath)
+    );
+    return result;
+}
+console.log(getDirFiles('src'));
