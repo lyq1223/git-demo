0862ff9819c3204b84f32a76c1062b5ff177b6ad
lyq1223 <56785439+lyq1223@users.noreply.github.com>
2020-04-30 21:38:57 +0800

    vue

--- index.js
+++ index.js
@@ -1,39 +1,28 @@
-var fs = require('fs'); // node file system node 自己的模块
-var path = require('path'); //路径
-// uuid npm 第三方
-// fs.readFile('aa.txt', function(err, data) {
-//   if (err) {
-//     console.log(err);
-//     return;
-//   }
-//   console.log(data.toString());
-// })
+import $ from 'jquery'
+import './css/1.css'
+import './css/1.less'
+import './css/1.scss'
+//导入单文件组件
+// import APP from './components/APP.vue'
 
-function getDirFiles(startPath) {
-    let result = [];
-    // 递归
-    function finder(parentPath) {
-        // console.log(path);
-        let files = fs.readdirSync(parentPath);
-        if (!files.length) return;
-        // console.log(files);
-        files.forEach(function (val, index) {
-            // console.log(val, index);
-            // 目录还是文件
-            let fPath = path.join(parentPath, val);
-            // console.log(fPath);
-            let stats = fs.statSync(fPath);
-            if (stats.isDirectory()) {
-                finder(fPath);
-            }
-            if (stats.isFile()) {
-                result.push(fPath);
-            }
-        })
-    }
-    finder(
-        path.join(process.cwd(), startPath)
-    );
-    return result;
+$(function () {
+    $('li:odd').css('backgroundColor', 'green')
+    $('li:even').css('backgroundColor', 'lightblue')
+})
+
+class Person {
+    static info = 'aaa'
 }
-console.log(getDirFiles('src'));
+
+console.log(Person.info)
+
+// --------------------------------
+// vue的基本使用步骤
+import Vue from 'vue'
+//导入单文件组件
+import App from './components/App.vue'
+
+const vm = new Vue({
+    el: '#app',
+    render: h => h(App)
+})
