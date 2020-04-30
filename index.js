c3c0fb06a3ffd71c8381f34d91b41f5ee1a803b7
lyq1223 <56785439+lyq1223@users.noreply.github.com>
2020-04-30 21:46:09 +0800

    es6

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-import $ from 'jquery'
-import './css/1.css'
-import './css/1.less'
-import './css/1.scss'
-//导入单文件组件
-// import APP from './components/APP.vue'
+import m1 from './m1.js'
 
-$(function () {
-    $('li:odd').css('backgroundColor', 'green')
-    $('li:even').css('backgroundColor', 'lightblue')
-})
-
-class Person {
-    static info = 'aaa'
-}
-
-console.log(Person.info)
-
-// --------------------------------
-// vue的基本使用步骤
-import Vue from 'vue'
-//导入单文件组件
-import App from './components/App.vue'
-
-const vm = new Vue({
-    el: '#app',
-    render: h => h(App)
-})
+console.log(m1)
